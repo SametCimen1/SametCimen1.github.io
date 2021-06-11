f7411801e790d4ac73d736eae6aa38ad74496f9e
SametCimen1 <cimensamet338@gmail.com>
2021-06-11 19:01:34 -0400

    Coming Soon

--- main.js
+++ main.js
@@ -1,59 +1,50 @@
-var elem = document.querySelectorAll(".myName");
-var links = document.querySelectorAll(".myLink");
-for(let i = 0; i<elem.length; i++){
-elem[i].addEventListener('mouseover', ()=>{
-  elem[i].classList.add("mouseMovedInside");
-});
- 
- 
-elem[i].addEventListener("mouseout", ()=>{
-  elem[i].style.opacity = 0;
-  elem[i].classList.add("mouseMovedOutside")
-  elem[i].classList.remove("mouseMovedInside");
-setTimeout(() => {
-   
-  elem[i].style.opacity = ''
-  }, 5)
-  elem[i].addEventListener("animationend", animationEnd);
-})
+var days = document.querySelector(".dayText");
+var hours = document.querySelector(".hourText");
+var minutes = document.querySelector(".MinuteText");
+var seconds = document.querySelector(".secondText");
+
+
+
+var release = new Date(2021, 5,27);
+var today = new Date();
+var milSeconds = Math.abs((release.getTime()-today.getTime()))
+let  diffSeconds =Math.floor(milSeconds/1000);
+let  diffMinutes =Math.floor(diffSeconds/60);
+let  diffHours =Math.floor(diffSeconds/3600);
+let  diffDays =Math.floor(diffHours/24);
+const upDateTime = () =>{
+  today = new Date();
+  milSeconds = Math.abs((release.getTime()-today.getTime()))
+  diffSeconds =Math.floor(milSeconds/1000);
+  diffMinutes =Math.floor(diffSeconds/60);
+  diffHours =Math.floor(diffSeconds/3600);
+  diffDays =Math.floor(diffHours/24);
+ days.textContent = diffDays;
+ hours.textContent = diffHours%24;
+ minutes.textContent = diffMinutes%60;
+ seconds.textContent = diffSeconds%60;
+}
+function update(){
+  
+  setInterval(upDateTime,1000);
 }
 
-for(let i = 0; i<links.length; i++){
-  links[i].addEventListener('mouseover', ()=>{
-    links[i].classList.add("mouseMovedInside");
-  });
+update();
+
+function createBubble(){
+
+  const area = document.querySelector(".bouble");
+    const createElem = document.createElement('span');
+    createElem.style.width = "1px";
+    createElem.style.height = "1px";
+    
+    createElem.style.left = Math.random() * (window.innerWidth-20) +"px";
    
-   
-  links[i].addEventListener("mouseout", ()=>{
-    links[i].style.opacity = 0;
-    links[i].classList.add("mouseMovedOutside")
-    links[i].classList.remove("mouseMovedInside");
-  setTimeout(() => {
-     
-    links[i].style.opacity = ''
-    }, 5)
-    links[i].addEventListener("animationend", animationEnd);
-  })
+    area.appendChild(createElem);
+    console.log()
+    setTimeout(()=>{
+      createElem.remove();
+    }, 10000)
   }
    
-   
- 
- 
- 
- 
- 
-function createBubble(){
-  const area = document.querySelector(".bouble");
-  const createElem = document.createElement('span');
-  createElem.style.width = "1px";
-  createElem.style.height = "1px";
-  createElem.style.left = Math.random() * innerWidth +"px";
- 
-  area.appendChild(createElem);
-  console.log()
-  setTimeout(()=>{
-    createElem.remove();
-  }, 10000)
-}
- 
 setInterval(createBubble,200)
