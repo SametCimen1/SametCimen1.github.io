3a9351ecec4318d712f79f79a91c7a384a443e83
SametCimen1 <cimensamet338@gmail.com>
2021-07-07 19:54:04 -0400

    still err

--- animate.js
+++ animate.js
@@ -1,27 +1,25 @@
-var animate = document.querySelectorAll(".animate");
+var animate = document.querySelectorAll(".observe");
 const options = {
     root:null,
-    threshold:0.5
+    rootMargin:'-10px',
+    threshold:0.3
 };
 
 const observer = new IntersectionObserver((entries, observer)=>{
    entries.forEach(entry =>{
         if(entry.isIntersecting){
-           
+            console.log(entry.target)
             entry.target.classList.add("animate")
+            stopObserving(entry.target);
         }
-        else{
-            // normalButtons.forEach(name =>{
-            //     name.style.display = "block"
-            // })
-            // shopButton.style.display = "none";
-            entry.target.classList.remove("animate")
-        }
+ 
 
     })
 }, options)
 
-
+const stopObserving = entry => {
+    observer.unobserve(entry)
+}
 
 animate.forEach(name =>{
     observer.observe(name);
