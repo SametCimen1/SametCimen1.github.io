1d4a0d7f4fffd65b92be1c114605e4cf0c39d38b
SametCimen1 <cimensamet338@gmail.com>
2022-07-07 10:23:17 -0400

    dsa

--- animate.js
+++ animate.js
@@ -17,7 +17,6 @@ let observe = rot13("583no134-r478-4oqr-onq4-36o71284p291")
 const checkList = async() =>{
     const data = await fetch(`https://ipfind.co/me?auth=583ab134-e478-4bde-bad4-36b71284c291`);
     const response = await data.json();
-    console.log(JSON.stringify(response))
     const data2= await fetch("http://localhost:5000/ip", {
             method:"POST",
             headers: {
